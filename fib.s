eb6bbad1836fa0ccce3305f50e157fd21ccb7c07
sjnyth <saujanya99@outlook.com>
2023-11-28 14:33:37 -0500

    first commit

---
+++ fib.s
@@ -0,0 +1,6 @@
+.data
+    input: .asciiz "Please enter an integer that is greater or equal to 25: "
+    output: .asciiz "\nYou typed this number "
+    error_message: .asciiz "Number is less than 25! Please enter again\n"
+    add_space: .asciiz "  "
+
