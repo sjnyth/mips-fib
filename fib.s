80f022b969b520c165ffdcd487ce1f5f3a63b7e4
sjnyth <saujanya99@outlook.com>
2023-12-03 14:32:11 -0500

    adding comments

--- fib.s
+++ fib.s
@@ -6,36 +6,43 @@
 
 .text
 main:
-    li $v0, 4   # calls prompt to take an input integer
+    # prompt user to enter an integer
+    li $v0, 4   # syscall code for printing string
     la $a0, input  # loads address and puts the interger value in register $a0
     syscall  
 
-    li $v0, 5  
+    # read integer input 
+    li $v0, 5  # system call code for reading integer
     syscall
-    move $t0, $v0
-    bgt $t0, 24, exit
-    li $v0, 4
+
+    # Check if the input is greater than or equal to 25
+    move $t0, $v0 # store user input in $t0
+    bgt $t0, 24, exit # branch if greater than or equal to 25
+    li $v0, 4 
     la $a0, error_message
     syscall
-    j main
+    j main # jump back to main if input is less than 25
 
 exit:
+    # Call the fib function with user input as argument
     move $a0, $v0
     jal fib
 
 fib:
+    # Fibonacci sequence calculation
     li $t0, 0
     li $t1, 1
     li $t2, 1
     move $t3, $a0
     sub $t4, $t3, 2
-    li $v0, 1
+    li $v0, 1   # system call code for printing integer
 
     li $t5, 0
-    beq $t3, 0, done
-    bne $t3, 1, else
+    beq $t3, 0, done    # if n is 0, jump to done
+    bne $t3, 1, else    # if n is 1, jump to else
 
 else:
+    # Print the first two Fibonacci numbers (0 and 1)
     li $v0, 1
     move $a0, $t0
     syscall
@@ -56,7 +63,8 @@ else:
     syscall
 
 loop:
-    beq $t5, $t4, done
+    # Fibonacci sequence calculation loop   
+    beq $t5, $t4, done  # if counter equals n-2, jump to done
     add $t0, $t1, $t2
     add $t1, $zero, $t2
     add $t2, $zero, $t0
@@ -70,5 +78,6 @@ loop:
     j loop
 
 done:
+    # Exit the program
     li $v0, 10
     syscall
